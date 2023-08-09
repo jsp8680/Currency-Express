a532ec3d75a7f3369a79add3a9f944205eb0a124
jsp8680 <97013156+jsp8680@users.noreply.github.com>
2023-08-09 14:37:17 -0600

    Added route and login page

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.use(express.static("public"));
 app.get("/", (req, res) => res.render("home.ejs"));
 
 app.get("/home", (req, res) => res.render("home.ejs"));
-
+app.get('/login', (req, res) => res.render('login.ejs'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
