c167bfcb098a33df23e41f81bbd56e5e104033d7
jsp8680 <97013156+jsp8680@users.noreply.github.com>
2023-08-10 14:15:28 -0600

    Fixed server

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.get("/", (req, res) => res.render("home.ejs"));
 app.use(Routes);
 
 // Start the server and listen on the specified port
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
