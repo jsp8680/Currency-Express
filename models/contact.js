de1c9514cb99b141ea5dcaa5a90f8ba38de034dd
rjwright92 <129555935+rjwright92@users.noreply.github.com>
2023-08-21 15:12:46 -0600

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 
 const contactSchema = new mongoose.Schema({
 
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    message: {type: String, required: true}
+    name: {
+      type: String,
+      required: [true, 'Full name is required'],
+      minlength: [7, 'Full name must be at least 7 characters.'],
+      maxlength: [25, 'Full name must be no longer than 25 charaters.'],
+    },
+    email: {
+      type: String, 
+      required: [true, 'Email is required'],
+      unique: true,
+      validate: [isEmail, 'Provide a vaild email address']
+    },
+    message: {
+      type: String, 
+      required: [true, 'Message is required']
+    }
   })
   
   module.exports = mongoose.model('Contact', contactSchema);
+
+
+  
