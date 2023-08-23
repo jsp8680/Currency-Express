ba9ccdd4c4c4bbdbcf1c2d8213f26d1ae24e3a4d
rjwright92 <129555935+rjwright92@users.noreply.github.com>
2023-08-23 14:22:08 -0600

    Add files via upload

---
+++ contact.test.js
@@ -0,0 +1,88 @@
+// command to run: npx mocha contact.test.js
+const { Builder, By, Key, until } = require("selenium-webdriver");
+require("chromedriver");
+const assert = require("chai").assert;
+
+describe("contact", function () {
+    let driver;
+
+    before(async function () {
+        driver = await new Builder().forBrowser("chrome").build();
+    });
+
+    after(async function () {
+        await driver.quit();
+    });
+
+    it("should fill out the contact form an submit (correct info)", function (done) {
+        this.timeout(10000);
+
+        (async () => {
+            await driver.get("https://currencyexpress.onrender.com/about");
+
+            // Fill out the form with correct login credentials
+            await driver.findElement(By.id("name")).sendKeys("test name")
+            await driver.findElement(By.id("email")).sendKeys("test@gmail.com");
+            await driver.findElement(By.id("message")).sendKeys("test message", Key.RETURN);
+            await driver.findElement(By.id("submit")).click();
+            
+            await driver.wait(until.urlIs("https://currencyexpress.onrender.com/"), 10000);
+
+            const currentUrl = await driver.getCurrentUrl();
+            assert.equal(currentUrl, "https://currencyexpress.onrender.com/", "Login was not successful");
+
+            done();
+        })().catch(done);
+    });
+
+    it("should display an error message for an invalid email", function (done) {
+        this.timeout(10000);
+
+        (async () => {
+            await driver.get("https://currencyexpress.onrender.com/about");
+
+            // Fill out the form with incorrect login credentials
+            await driver.findElement(By.id("name")).sendKeys("test");
+            await driver.findElement(By.id("email")).sendKeys("incorrect email");
+            await driver.findElement(By.id("message")).sendKeys("testmessage");
+            await driver.findElement(By.id("submit")).click();
+           
+
+            // Assuming the page displays an error message element, wait for it to appear
+            await driver.wait(until.elementLocated(By.className("email error")), 10000);
+
+            // Assert that the error message element is displayed
+            const errorMessage = await driver.findElement(By.className("email error"));
+            await driver.wait(until.elementIsVisible(errorMessage), 10000);
+            assert.isTrue(await errorMessage.isDisplayed(), "Error message not displayed");
+
+            done();
+        })().catch(done);
+    });  
+    
+    it("should display an error message for too little or too much charaters for name", function (done) {
+        this.timeout(10000);
+
+        (async () => {
+            await driver.get("https://currencyexpress.onrender.com/about");
+
+            // Fill out the form with incorrect login credentials
+            await driver.findElement(By.id("name")).sendKeys("test");
+            await driver.findElement(By.id("email")).sendKeys("test@example.com");
+            await driver.findElement(By.id("message")).sendKeys("test message");
+            await driver.findElement(By.id("submit")).click();
+           
+
+            // Assuming the page displays an error message element, wait for it to appear
+            await driver.wait(until.elementLocated(By.className("name error")), 5000);
+
+            // Assert that the error message element is displayed
+            const errorMessage = await driver.findElement(By.className("name error"));
+            await driver.wait(until.elementIsVisible(errorMessage), 5000);
+            assert.isTrue(await errorMessage.isDisplayed(), "Error message not displayed");
+
+            done();
+        })().catch(done);
+    });
+});
+
