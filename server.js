3b3b57cfbc6962978aeea2367cb451501538b280
Jolene Parker <97013156+jsp8680@users.noreply.github.com>
2023-08-31 10:28:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -40,10 +40,7 @@ const encodedUsername = encodeURIComponent(username);
 const encodedPassword = encodeURIComponent(password);
 // // Connect to the MongoDB database using Mongoose
 const dbURI = `mongodb+srv://${encodedUsername}:${encodedPassword}@cluster0.e4ob5c0.mongodb.net/`;
-// // const dbURI = 'mongodb+srv://rjwright929:hello123@cluster0.dhfxvb8.mongodb.net/contact';
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then((result) => app.listen(3000),console.log('Server is running on port 3000 http://localhost:3000/'))
-//   .catch((err) => console.log(err));
+
 //Set connection params
 const connectionParams={
   useNewUrlParser: true,
